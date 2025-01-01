8cb5b5d482b9acc510ebf92285b7ec7b82434af8
Thisara Chathuranga <thisarachathuranga000@gmail.com>
2025-01-01 14:57:25 +0530

    new changes

---
+++ app/index.tsx
@@ -0,0 +1,38 @@
+import { Text, View } from "react-native";
+import { Image, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+          source={require('@/assets/images/logo.png')}
+          style={styles.reactLogo}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
